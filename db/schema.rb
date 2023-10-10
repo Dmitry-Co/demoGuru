f236a33b1972dcfd8dedce2df0f44f0709248c67
Dmitry-Co <sechko.dp@gmail.com>
2023-10-10 12:17:59 +0500

    fix schema for User models

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_050812) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "555", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
